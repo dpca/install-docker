0314209c1c1ce6ee2235fb02ec1d33bf4c90f1af
Dan Cahoon <dpcahoon@gmail.com>
2016-06-02 15:30:57 -0400

    Optionally install docker-compose

--- install_docker.sh
+++ install_docker.sh
@@ -14,7 +14,7 @@ update_apt_sources() {
   apt-get install -y apt-transport-https ca-certificates
   apt-key adv --keyserver hkp://p80.pool.sks-keyservers.net:80 --recv-keys 58118E89F3A912897C070ADBF76221572C52609D
 
-  case "$version" in
+  case $version in
     "14.04" ) entry="deb https://apt.dockerproject.org/repo ubuntu-trusty main";;
     "15.10" ) entry="deb https://apt.dockerproject.org/repo ubuntu-wily main";;
     "16.04" ) entry="deb https://apt.dockerproject.org/repo ubuntu-xenial main";;
@@ -43,15 +43,34 @@ install_docker() {
   service docker start
 }
 
+install_compose() {
+  curl -L https://github.com/docker/compose/releases/download/latest/docker-compose-`uname -s`-`uname -m` > /usr/local/bin/docker-compose
+  chmod +x /usr/local/bin/docker-compose
+}
+
 if [ "$(whoami)" != "root" ]; then
   error "Please run as root"
   exit 1
 fi
 
+# parse command line arguments
+while [[ $# > 0 ]]
+do
+key="$1"
+case $key in
+  --install-compose) INSTALL_COMPOSE=1;;
+  *) ;;
+esac
+shift
+done
+
 if [ "$version" == "14.04" ] || [ "$version" == "15.10" ] || [ "$version" == "16.04" ]; then
   update_apt_sources
   install_prereqs
   install_docker
+  if [ "$INSTALL_COMPOSE" == 1 ]; then
+    install_compose
+  fi
 else
   error "Ubuntu $version not supported"
   exit 1
